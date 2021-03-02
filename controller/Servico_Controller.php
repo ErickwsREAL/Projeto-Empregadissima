647e8b135f11064eb4629a67d57b22f07b8a266e
bruna <brunam.silva01@gmail.com>
2021-03-02 20:44:43 -0300

    Criação de classe ServicoDAO, correção bug na tela de serviço (contratante)

--- controller/Servico_Controller.php
+++ controller/Servico_Controller.php
@@ -1,28 +1,42 @@
 <?php include ("../login_control/logar_bd_empregadissimas.php") ?>
 <?php
 
-    require_once '../model/Servico.php';
-    
+    require_once ('../model/Servico.php');
+    require_once ('../DAO/ServicoDAO.php');
 
     //$id_diaria = $_POST['res']; 
 
     switch($_GET['metodo']){
         
         case 'inserir':
-            Servico::insert($_POST, $_GET['data_servico']);
+            $servico = new Servico();
 
+            $servico->setIdEndereco($_POST['id_endereco']);
+            $servico->setFormaPagamento($_POST['forma_pagamento']);
+            $servico->setIdPrestador($_POST['id_prestador']);
+            $servico->setIdContratante($_POST['id_contratante']);
+            $servico->setIdDiaria($_POST['id_diaria']);
+            $servico->setDataServico($_GET['data_servico']);
+            
+            $servicoDAO = new ServicoDAO();
+            $check = $servicoDAO->insert($servico);
+
+            //Servico::insert($_POST, $_GET['data_servico']);
+            if ($check == 1) {
+                echo '<script>alert("Seu cadastro não foi efetuado! Tente novamente.")</script>';
+            }
             echo '<script>alert("Solicitação de Serviço enviada com sucesso!")</script>';
             echo '<script>location.href="../views/manter-solicitacao-contratante.php"</script>';
             break;
         case 'deletar':
 
-            Servico::delete($_GET['id_diaria'], $_POST);
+            ServicoDAO::delete($_GET['id_diaria'], $_POST);
 
             echo '<script>alert("Serviço deletado com sucesso!")</script>';
             echo '<script>location.href="../views/perfil.php"</script>';
             break;
         case 'buscar':
-            $parametros = Servico::select($_POST,$_GET['id_servico']);
+            $parametros = ServicoDAO::select($_POST,$_GET['id_servico']);
 
             $valores = array();
             $valores['data_servico'] = $parametros['data_servico'];
@@ -40,22 +54,39 @@
             break;
         case 'atualizar':
 
-            Servico::update($_GET, $_POST);
+            $servico = new Servico();
+            $servico->setIdEndereco($_POST['id_endereco']);
+            $servico->setFormaPagamento($_POST['forma_pagamento']);
+            $servico->setIdPrestador($_POST['id_prestador']);
+            $servico->setIdContratante($_POST['id_contratante']);
+            $servico->setIdDiaria($_GET['id_diaria']);
+            $servico->setDataServico($_GET['data_servico']);
+
+            $servicoDAO = new ServicoDAO();
+            
+            $servicoDAO->updateDAO($servico, $_GET);
+
+        //    ServicoDAO::update($_GET, $_POST);
             echo '<script>alert("Serviço atualizado com sucesso!")</script>';
             echo '<script>location.href="../views/perfil.php"</script>';    
             break;   
         case 'alt_status_apv':
-            Servico::aprovaServico($_POST, $_GET['id_servico']);
+            ServicoDAO::aprovaServico($_POST, $_GET['id_servico']);
 
 
             echo '<script>alert("Serviço aprovado com sucesso!")</script>';
             echo '<script>location.href="../views/manter-solicitacao.php"</script>';
             break;
         case 'alt_status_rep':
-            Servico::reprovaServico($_POST, $_GET['id_servico']);
+            ServicoDAO::reprovaServico($_POST, $_GET['id_servico']);
+            
+            if ($_GET['tipo_pessoa'] == 1){
+                echo '<script>location.href="../views/manter-solicitacao-contratante.php"</script>';
+            }
+            else{
+                echo '<script>location.href="../views/manter-solicitacao.php"</script>';
+            }
 
-            echo '<script>alert("Serviço reprovado com sucesso!")</script>';
-            echo '<script>location.href="../views/manter-solicitacao.php"</script>';
              break; 
     }
 
--- model/Servico.php
+++ model/Servico.php
@@ -1,91 +1,64 @@
 <?php
-    class Servico{    
-        public static function insert($dadosServico, $data_servico){
-           include ("../controller/login_control/logar_bd_empregadissimas.php");
+    class Servico{  
 
-           $id_endereco = $dadosServico['id_endereco'];
-           $forma_pagamento = $dadosServico['forma_pagamento'];
-           $id_prestador = $dadosServico['id_prestador'];
-           $id_contratante = $dadosServico['id_contratante'];
-           $id_diaria = $dadosServico['id_diaria'];
+      /*
+      * Classe Servico, Envia informações da entidade Servico para a Classe ServicoDAO 
+      */ 
 
-           $data_servico = date($data_servico);
+      protected $id_diaria;    
+      protected $id_prestador;
+      protected $id_contratante;    
+      protected $id_endereco;
+      protected $forma_pagamento;
+      protected $data_servico;
 
-           $sql = "INSERT INTO servico (data_servico, id_endereco, forma_pagamento, status_servico, id_prestador, id_contratante, id_diaria) VALUES (STR_TO_DATE('$data_servico', '%Y/%m/%d'),'$id_endereco', '$forma_pagamento', '1', '$id_prestador', '$id_contratante', '$id_diaria')";
+      public function getIdDiaria() {
+        return $this->id_diaria;
+      }
 
-           $conn->query($sql);
-            
-            if ($conn->query($sql) === TRUE) {
-              echo "New record created successfully";
-            } else {
-              echo "Error: " . $sql . "<br>" . $conn->error;
-            }
+      public function setIdDiaria($id_diaria) {
+        $this->id_diaria = $id_diaria;
+      }
 
-            $conn->close();
-        }
-        public static function aprovaServico($dadosServico, $id_servico){
-          include("../controller/login_control/logar_bd_empregadissimas.php");
+      public function getIdPrestador() {
+        return $this->id_prestador;
+      }
 
-          echo $id_servico;
+      public function setIdPrestador($id_prestador) {
+        $this->id_prestador = $id_prestador;
+      }
 
-          $sql = "UPDATE servico SET status_servico = '2' WHERE (id_servico = '$id_servico')";
+      public function getIdContratante() {
+        return $this->id_contratante;
+      }
+      
+      public function setIdContratante($id_contratante) {
+        $this->id_contratante = $id_contratante;
+      }
 
-          $conn->query($sql);
-            
-            if ($conn->query($sql) === TRUE) {
-              echo "New record created successfully";
-            } else {
-              echo "Error: " . $sql . "<br>" . $conn->error;
-            }
+      public function getIdEndereco() {
+        return $this->id_endereco;
+      }
 
-            $conn->close();
-        }
+      public function setIdEndereco($id_endereco) {
+          $this->id_endereco = $id_endereco;
+      }
+      
+      public function getFormaPagamento() {
+          return $this->forma_pagamento;
+      }
 
-        public static function reprovaServico($dadosServico, $id_servico){
-          include("../controller/login_control/logar_bd_empregadissimas.php");
+      public function setFormaPagamento($forma_pagamento) {
+          $this->forma_pagamento = $forma_pagamento;
+      }
 
-          $sql = "DELETE FROM servico WHERE (id_servico = '$id_servico')";
-
-          $conn->query($sql);
-            
-            if ($conn->query($sql) === TRUE) {
-              echo "New record updated successfully";
-            } else {
-              echo "Error: " . $sql . "<br>" . $conn->error;
-            }
-
-            $conn->close();
-        }
-
-         public static function select($dadosGET, $id_servico){
-            include ("../controller/login_control/logar_bd_empregadissimas.php");
-
-            $sql = "SELECT data_servico, id_endereco, forma_pagamento, id_diaria FROM servico WHERE id_servico='$id_servico'";
-
-            $result = $conn->query($sql);
-
-            $row = $result->fetch_assoc();
-
-            $dados = array(
-                'data_servico' => $row["data_servico"],
-                'id_endereco' => $row["id_endereco"],
-                'forma_pagamento' => $row["forma_pagamento"],
-                'id_diaria' => $row["id_diaria"]
-            );
-
-            if ($result->num_rows > 0) {
-                /*output data of each row*/
-              while($row = $result->fetch_assoc()) {
-                echo "data_servico: " . $row["data_servico"]. " - id_endereco: " . $row["id_endereco"]. "<br>";
-              }
-            } else {
-               echo "0 results";
-            }
-          
-            $conn->close();
-            
-            return $dados;
-        }
-    }
+      public function getDataServico() {
+        return $this->data_servico;
+      }
+      
+      public function setDataServico($data_servico) {
+        $this->data_servico = $data_servico;
+      }   
+  }
 
 ?>
